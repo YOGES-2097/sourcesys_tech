c209fa0772bacc1d2ab9258c6b9845a3e39c032d
YOGES-2097 <yogeswaranm2907@gmail.com>
2026-02-27 11:31:53 +0530

    Improved data simulation logic

--- numpy test.py
+++ numpy test.py
@@ -1,17 +1,35 @@
 import numpy as np
 import pandas as pd
 
-arr = np.array([10, 20, 30, 40, 50])
-print("Array:", arr)
-print("Mean:", np.mean(arr))
+#arr = np.array([10, 20, 30, 40, 50])
+#print("Array:", arr)
+#print("Mean:", np.mean(arr))
 
-data = {
-    "Name": ["Yoges", "Arun", "Kumar"],
-    "Marks": [85, 90, 78]
-}
+#data = {
+#    "Name": ["Yoges", "Arun", "Kumar"],
+#    "Marks": [85, 90, 78]
+#}
 
-df = pd.DataFrame(data)
-print("\nStudent Data:")
+#df = pd.DataFrame(data)
+#print("\nStudent Data:")
+#print(df)
+
+#print("\nAverage Marks:", df["Marks"].mean())
+
+## 2 example task
+np.random.seed(0)
+
+days = 30
+start_price = 100
+
+daily_returns = np.random.normal(0.001, 0.02, days)
+
+price_series = start_price * np.cumprod(1 + daily_returns)
+
+df = pd.DataFrame({
+    "Day": range(1, days + 1),
+    "Stock_Price": price_series
+})
+
 print(df)
-
-print("\nAverage Marks:", df["Marks"].mean())
+print("\nFinal Price:", df["Stock_Price"].iloc[-1])
